e59e683bf366b377d30b3b938102bb8bbc677521
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-29 21:28:46 +0300

    lesson 4 ok

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import { type RuleSetRule } from 'webpack';
 
 export const buildLoaders = (): RuleSetRule[] => {
+  const styleLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
+
   const tsLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
 
-  return [tsLoader];
+  return [tsLoader, styleLoader];
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import './style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div>Hello</div>);
