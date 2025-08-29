17a119ee706984290d7df33f9bbf156e27afa0c2
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-29 18:31:08 +0300

    1:13

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('node:path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };
