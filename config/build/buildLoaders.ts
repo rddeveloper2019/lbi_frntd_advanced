52052ac5d8e23115b032d48647e8e60a85ee9a9b
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-29 22:20:09 +0300

    lesson 5

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,9 +5,31 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 export const buildLoaders = (options: BuildOptions): RuleSetRule[] => {
   const styleLoader = {
     test: /\.s[ac]ss$/i,
+    exclude: /\.module\.s[ac]ss$/,
     use: [
       options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       'css-loader',
+      'sass-loader',
+    ],
+  };
+
+  const styleModuleLoader = {
+    test: /\.module\.s[ac]ss$/,
+    use: [
+      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          esModule: false,
+          modules: {
+            localIdentName: options.isDev
+              ? '[path]__[local]--[hash:base64:5]'
+              : '[hash:base64:12]',
+          },
+          importLoaders: 1,
+          sourceMap: options.isDev,
+        },
+      },
       'sass-loader',
     ],
   };
@@ -18,5 +40,5 @@ export const buildLoaders = (options: BuildOptions): RuleSetRule[] => {
     exclude: /node_modules/,
   };
 
-  return [tsLoader, styleLoader];
+  return [tsLoader, styleLoader, styleModuleLoader];
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './style.scss';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello</div>);
+root.render(<App />);
