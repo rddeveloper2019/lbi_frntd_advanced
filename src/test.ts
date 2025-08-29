cd20c66a473cbc7ed6784ecc57576fda480b55c5
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-29 20:30:26 +0300

    add react react-dom

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export const log = (time: number) => {
-  console.log('Now:', new Date(time).toTimeString());
-};
-
-document.body.innerHTML += '<div>Hello</div>';
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,7 +6,7 @@ import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
     html: path.resolve(__dirname, 'public', 'index.html'),
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
   };
 
