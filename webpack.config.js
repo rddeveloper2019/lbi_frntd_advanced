63905119eb74a66db486a8d4b46600da59d28ce8
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-29 18:20:24 +0300

    1:7

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('node:path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+};
