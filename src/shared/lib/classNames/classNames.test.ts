27ce71af4db5d94485c27e3b81e07f53fb1a83fa
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-31 15:37:01 +0300

    20 add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('Test classNames single arg', () => {
+    expect(classNames('single')).toBe('single');
+  });
+
+  test('Test classNames double arg', () => {
+    expect(classNames('test', 'test-2')).toBe('test test-2');
+  });
+
+  test('Test classNames with conditional arg', () => {
+    expect(classNames({ display: true, hide: false })).toBe('display');
+  });
+
+  test('Test classNames with undefined arg', () => {
+    expect(classNames()).toBe('');
+  });
+});
