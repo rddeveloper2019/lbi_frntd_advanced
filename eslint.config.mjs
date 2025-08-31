1b5899e5a8e6a345cd8af1447a2c34a86ca0480f
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-31 13:48:27 +0300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,37 +4,43 @@ import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: './tsconfig.json',
+                sourceType: 'module',
+            },
         },
-        project: './tsconfig.json',
-        sourceType: 'module',
-      },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        plugins: {
+            react: pluginReact,
+        },
     },
-    settings: {
-      react: {
-        version: 'detect', // ✅ Авто-определение версии React
-      },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            // 'react/jsx-indent': [2, 4],
+            // 'react/jsx-indent-props': [2, 4],
+            // indent: [2, 4],
+            // 'react/jsx-filename-extension': [
+            //     2,
+            //     { extensions: ['.js', '.jsx', '.tsx'] },
+            // ],
+        },
     },
-    plugins: {
-      react: pluginReact,
-    },
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended, // ← сначала подключаем
-  {
-    // 🔥 А потом — переопределяем правило
-    rules: {
-      'react/react-in-jsx-scope': 'off', // ✅ Теперь точно отключено
-    },
-  },
 ]);
