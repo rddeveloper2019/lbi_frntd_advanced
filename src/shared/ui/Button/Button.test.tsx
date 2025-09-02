4c953718a18f579ed72e58fc20f7c87fdef6615a
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-02 06:46:16 +0300

    24  add Button test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
-test('loads and displays greeting', async () => {
-  render(<Button>Hello</Button>);
+describe('Button tests', () => {
+  test('toBeInTheDocument', async () => {
+    render(<Button>Hello</Button>);
 
-  expect(screen.getByRole('button')).toBeInTheDocument();
-  expect(screen.getByText('Hello')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByText('Hello')).toBeInTheDocument();
+  });
+
+  test('toHave Class', async () => {
+    render(<Button theme={ThemeButton.CLEAR}>Hello</Button>);
+
+    expect(screen.getByText('Hello')).toHaveClass('clear');
+    screen.debug();
+  });
 });
