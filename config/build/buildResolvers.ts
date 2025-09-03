7db27a9360abe68509638ab31f9ceb032066b1df
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-03 22:11:03 +0300

    Update buildResolvers.ts

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,7 +3,7 @@ import { BuildOptions } from './types/config';
 
 export const buildResolve = (options: BuildOptions): ResolveOptions => {
   return {
-    extensions: ['.tsx', '.ts', '.js', 'jsx'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
     preferAbsolute: true,
     mainFiles: ['index'],
     modules: [options.paths.src, 'node_modules'],
