2a08cb552dd851cbc5f1ab283def040190c2a5a1
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-03 21:06:27 +0300

    rename button style path

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,5 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './Button.module.scss';
+import cls from './Button-styles.module.scss';
 
 import { ButtonHTMLAttributes, FC } from 'react';
 
