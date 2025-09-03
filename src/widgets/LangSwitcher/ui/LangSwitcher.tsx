b525d1c15036086b2a2f58b63ba7630f0aa523ae
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-03 22:14:56 +0300

    Update LangSwitcher.tsx

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './LangSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ThemeButton } from '../../../shared/ui/Button/Button';
 
 interface LangSwitcherProps {
   className?: string;
