f61f0a620da883eef465b21dabc726c29e9f40e0
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-03 22:27:17 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -40,6 +40,7 @@ export default defineConfig([
 
   {
     rules: {
+      'react/display-name': 'off',
       'react/react-in-jsx-scope': 'off',
 
       'i18next/no-literal-string': [
